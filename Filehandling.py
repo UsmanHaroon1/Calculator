0f5c946ac5821e47e2f74a09b68bdf6247dd0428
UsmanHaroon1 <haroonasr9@gmail.com>
2019-01-30 22:21:06 +0530

    Delete Filehandling.py

--- Filehandling.py
+++
@@ -1,8 +0,0 @@
-file = open("G:\\test.txt",'w')
-file.write("This is working,pyhton file handling exercise")
-file.close()
-
-file = open("G:\\test.txt",'r')
-content = file.read()
-print(content)
-file.close()
